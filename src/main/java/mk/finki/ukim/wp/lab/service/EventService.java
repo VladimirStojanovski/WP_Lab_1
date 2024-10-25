ce64ffe07926ca4795fda8a294e50131005915ff
Vladimir <vladimir.stojanovski5@gmail.com>
2024-10-25 18:40:13 +0200

    EventBookingService + EventService interface

---
+++ src/main/java/mk/finki/ukim/wp/lab/service/EventService.java
@@ -0,0 +1,9 @@
+package mk.finki.ukim.wp.lab.service;
+
+import mk.finki.ukim.wp.lab.model.Event;
+import java.util.List;
+
+public interface EventService {
+    List<Event> listAll();
+    List<Event> searchEvents(String text);
+}
