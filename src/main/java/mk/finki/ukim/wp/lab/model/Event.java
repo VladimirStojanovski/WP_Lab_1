67d2fe2c4fbdfd4a0aa1cff1bc08b499c2a24475
Vladimir <vladimir.stojanovski5@gmail.com>
2024-10-25 18:16:12 +0200

    EventBooking + Event classes step 2 + step 3

---
+++ src/main/java/mk/finki/ukim/wp/lab/model/Event.java
@@ -0,0 +1,9 @@
+package mk.finki.ukim.wp.lab.model;
+
+public class Event {
+    private String name;
+    private String description;
+    private double popularityScore;
+
+//  TODO getters setters Constructors
+}
---
+++ src/main/java/mk/finki/ukim/wp/lab/model/EventBooking.java
@@ -0,0 +1,10 @@
+package mk.finki.ukim.wp.lab.model;
+
+public class EventBooking {
+    private String eventName;
+    private String attendeeName;
+    private String attendeeAddress;
+    private Long numberOfTickets;
+
+//  TODO getters setters Constructors
+}
