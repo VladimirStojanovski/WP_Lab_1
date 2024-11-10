b1632b720614ed4d3ecc65da5fb3d9b1f6cff5a2
Vladimir <vladimir.stojanovski5@gmail.com>
2024-11-10 23:03:42 +0100

    step 3 adding location class

---
+++ src/main/java/mk/finki/ukim/wp/lab/model/Location.java
@@ -0,0 +1,9 @@
+package mk.finki.ukim.wp.lab.model;
+
+public class Location {
+    private Long id;
+    private String name;
+    private String address;
+    private String capacity;
+    private String description;
+}
